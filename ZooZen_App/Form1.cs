132cabe8a3f766294e21ceec1ff8b68f7cca92f1
VarghaLevi <levente.vargha.com>
2023-05-09 19:07:53 +0200

    Add project files.

---
+++ ZooZen_App/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.CommerceDTO;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1.Contacts;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Newtonsoft.Json;
+
+
+namespace ZooZen_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        
+        
+    }
+}
